dee68e963a5de29c7d41f9ccbb92bff46f77e3bd
Tentorato <rounakmanna64@gmail.com>
2025-02-01 01:51:40 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -103,13 +103,14 @@ async def start(client, message):
         try:
             btn = await is_subscribed(client, message, AUTH_CHANNEL)
             if btn:
-                if message.command[1]:
-                    username = (await client.get_me()).username
-                    btn.append([InlineKeyboardButton("♻️ Try Again ♻️", url=f"https://t.me/{username}?start={message.command[1]}")])
-                else:                   
-                    kk, file_id = message.command[1].split("_", 1)                    
-                    btn.append([InlineKeyboardButton("♻️ Try Again ♻️", callback_data=f"checksub#{kk}#{file_id}")])
-                await message.reply_text(text=f"<b>👋 Hello {message.from_user.mention},\n\nPlease join the channel then click on try again button. 😇</b>", reply_markup=InlineKeyboardMarkup(btn))             
+                if message.command[1] != "subscribe":
+                    try:                        
+                        kk, file_id = message.command[1].split("_", 1)
+                        btn.append([InlineKeyboardButton("♻️ Try Again ♻️", callback_data=f"checksub#{kk}#{file_id}")])
+                    except (IndexError, ValueError):
+                        username = (await client.get_me()).username
+                        btn.append([InlineKeyboardButton("♻️ Try Again ♻️", url=f"https://t.me/{username}?start={message.command[1]}")])
+                await message.reply_text(text=f"<b>👋 Hello {message.from_user.mention},\n\nPlease join the channel then click on try again button. 😇</b>", reply_markup=InlineKeyboardMarkup(btn))          
                 return
         except Exception as e:
             print(e)            
