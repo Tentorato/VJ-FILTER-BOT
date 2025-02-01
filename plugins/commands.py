ca0fbd0046d3d08765bd0104fabea44e84cc4522
Tentorato <rounakmanna64@gmail.com>
2025-02-01 13:14:42 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -107,7 +107,7 @@ async def start(client, message):
                 if message.command[1]:
                     btn.append([InlineKeyboardButton("♻️ Try Again ♻️", url=f"https://t.me/{username}?start={message.command[1]}")])
                 else:
-                    btn.append([InlineKeyboardButton("♻️ Try Again ♻️", callbackquery.answer ("Join Update Channel Then Try Again...☠️", show_alert = True))])
+                    btn.append([InlineKeyboardButton("♻️ Try Again ♻️", callback_data="callbackquery.answer ("Join Update Channel Then Try Again...☠️", show_alert = True)")])
                 await message.reply_text(text=f"<b>👋 Hello {message.from_user.mention},\n\nPlease join the channel then click on try again button. 😇</b>", reply_markup=InlineKeyboardMarkup(btn))
                 return
         except Exception as e:
