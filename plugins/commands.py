9ed0d671b7a0863079626916001cd392d19703b9
Tentorato <rounakmanna64@gmail.com>
2025-02-01 09:09:00 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -103,10 +103,9 @@ async def start(client, message):
         try:
             btn = await is_subscribed(client, message, AUTH_CHANNEL)
             if btn:
-                if message.command[1] != "subscribe":
-                    try:                        
-                        kk, file_id = message.command[1].split("_", 1)
-                        btn.append([InlineKeyboardButton("♻️ Try Again ♻️", callback_data=f"checksub#{kk}#{file_id}")])
+                if message.command[1] != "subscribe":                    
+                    kk, file_id = message.command[1].split("_", 1)
+                    btn.append([InlineKeyboardButton("♻️ Try Again ♻️", callback_data=f"checksub#{kk}#{file_id}")])
                 else:                    
                     username = (await client.get_me()).username
                     btn.append([InlineKeyboardButton("♻️ Try Again ♻️", url=f"https://t.me/{username}?start={message.command[1]}")])
